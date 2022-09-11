91696aaf76200f78cc69573bdee726f5877f63f6
Ankit Dahiya <ankitdahiya2002@gmail.com>
2022-09-11 19:36:27 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react"
+import ReactDOM from "react-dom"
+ReactDOM.render(
+    <h1>My favorite food </h1>,
+    document.getElementById("root"));
+
